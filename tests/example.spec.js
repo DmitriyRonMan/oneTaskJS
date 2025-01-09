ad76307b960a82afd0d78e39d40283d240a2b68f
Dmitriy RonMan <dmitriykolesnikov7@gmail.com>
2025-01-09 16:02:27 +0500

    change test

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,19 +1,14 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('test', async ({ page }) => {
+  await page.goto('https://realworld.qa.guru/');
+  await page.getByRole('link', { name: 'Sign up' }).click();
+  await page.getByPlaceholder('Your Name').click();
+  await page.getByPlaceholder('Your Name').fill('Dmitriy');
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill('guruTest93@example.com');
+  await page.getByPlaceholder('Password').click();
+  await page.getByPlaceholder('Password').fill('qwertyuio');
+  await page.getByRole('button', { name: 'Sign up' }).click();
+  await expect(page.getByText('Dmitriy')).toBeVisible();
 });
